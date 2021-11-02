1238eee50d6bfb3f7b0c4f1cc87cdeef241fcb34
Swaraj Saxena <72065474+swarajsaxena@users.noreply.github.com>
2021-11-03 00:44:43 +0530

    1st commit

---
+++ precache-manifest.a0a7d1f3ac20754515b370e877afc7c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbaf7e2ae53865192653618241a12350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e72298be94902599993e",
+    "url": "/static/css/main.26bd57f0.chunk.css"
+  },
+  {
+    "revision": "dfa9608f2086ed0e8bf6",
+    "url": "/static/js/2.e576e615.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/static/js/2.e576e615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509cd923f0fe17a2c0bc",
+    "url": "/static/js/3.26cb0be1.chunk.js"
+  },
+  {
+    "revision": "e72298be94902599993e",
+    "url": "/static/js/main.18053bb9.chunk.js"
+  },
+  {
+    "revision": "e03ab7ae7330ff328c01",
+    "url": "/static/js/runtime-main.dff03738.js"
+  }
+]);
